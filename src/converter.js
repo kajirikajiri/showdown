f92db3cf89248c0f2971710f0b27a03e5a1f7c19
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-07-26 16:00:55 +0900

    Quick update

--- src/converter.js
+++ src/converter.js
@@ -351,7 +351,7 @@ showdown.Converter = function (converterOptions) {
     // update metadata
     metadata = globals.metadata;
     if (options.returnObject) {
-      return {text: text, timeList: globals.timeList};
+      return {html: text, timeList: globals.timeList};
     } else {
       return text;
     }
